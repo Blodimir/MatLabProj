98b4622063f908d0c2a2775fbf7b3c555af1f8fd
Blodimir <Blodimir.yar@gmail.com>
2016-10-29 17:27:56 +0300

    Завершена лабораторная работа по обработке сигналов. 29.10.16.

---
+++ FFT_Melnikov/LabaSatuday.m
@@ -0,0 +1,11 @@
+clear all;
+for i=1:22
+    a(i)=1;
+end;
+for i=23:46
+    a(i)=0;
+end
+for i=47:64
+    a(i)=1;
+end
+plot(a);
--- FFT_Melnikov/test.m
+++ FFT_Melnikov/test.m
@@ -1,9 +1,14 @@
+N=52;
 f = cos(2*pi/N*(0:N-1)*5);
 l =(cos(2*pi/N*(0:N-1)*10)/3).^2;
 n=(0:1:10).^2;
-subplot(211);
-plot(f);%Отрисовка исходной функции
-subplot(212);
+w=hamming(N);
+%subplot(211);
+%plot(f);%Отрисовка исходной функции
+%subplot(212);
 %hold off;
-plot(n);
+%plot(n);
 %hold on 
+%subplot(213);
+%hold off;
+plot(w);
