a15274dead29be5767617f9ae551e3b2abc8de20
Blodimir <Blodimir.yar@gmail.com>
2016-11-20 00:49:55 +0300

    Modified

--- Image_sdwig/Untitled.m
+++ Image_sdwig/Untitled.m
@@ -3,18 +3,19 @@ x=imshow('warp.jpg');
 w = getimage;
 q=0; %z=zeros(250,1250,3);
 for i=1:1:250
-    for j=1:1:1000 
-        w=j;
+    for j=1:1:750 
+        e=j;
          for k=1:1:3 
-             if (w+q)>1000
-               w=-q;
-             end
-             z(i,j,k) = w(i,w+q,k);
+             %if (e+q)>1000
+            %   e=-q;
+            % end
+            b=e+q;
+             z(i,j,k) = w(i,b,k);
          end
     end
-    if mod(i,5)==0
-     q=q+5;
+    if mod(i,8)==0
+     q=q+8;
     end
 end
-z=rot90(z);
+%z=rot90(z);
 subimage(z);
