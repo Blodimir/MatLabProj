b3d3d72d69678aafc66f26159b4709d42c05c828
Blodimir <Blodimir.yar@gmail.com>
2016-11-20 01:07:28 +0300

    it's work...

--- Image_sdwig/Untitled.m
+++ Image_sdwig/Untitled.m
@@ -1,16 +1,21 @@
 clear all;
 x=imshow('warp.jpg');
 w = getimage;
-q=0; %z=zeros(250,1250,3);
+q=0; e=0; 
+%z=zeros(250,1000,3);
 for i=1:1:250
-    for j=1:1:750 
-        e=j;
+    for j=1:1:1000 
+        %e=j;
          for k=1:1:3 
-             %if (e+q)>1000
-            %   e=-q;
-            % end
-            b=e+q;
-             z(i,j,k) = w(i,b,k);
+            if (j+q)>1000
+               e=-q;
+            end
+            if j<=1000-q 
+             z(i,j,k) = w(i,j+q,k);
+            end
+            if j>1000-q 
+             z(i,j,k) = w(i,j-1000+q,k);
+            end 
          end
     end
     if mod(i,8)==0
