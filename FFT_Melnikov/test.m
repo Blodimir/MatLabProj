b1f611ca9a4e5cadae933ceaf5a8f3b57ec1235b
Blodimir <Blodimir.yar@gmail.com>
2016-11-26 16:40:49 +0300

    okay

--- FFT_Melnikov/test.m
+++ FFT_Melnikov/test.m
@@ -1,14 +1,18 @@
-N=52;
+clear all;
+N=52; m=3;
+
+si=cos(2*pi/1:0.1:N);
+
 f = cos(2*pi/N*(0:N-1)*5);
 l =(cos(2*pi/N*(0:N-1)*10)/3).^2;
 n=(0:1:10).^2;
 w=hamming(N);
-%subplot(211);
-%plot(f);%Отрисовка исходной функции
+subplot(211);
+plot(si);%Отрисовка исходной функции
 %subplot(212);
 %hold off;
 %plot(n);
 %hold on 
-%subplot(213);
+subplot(212);
 %hold off;
 plot(w);
